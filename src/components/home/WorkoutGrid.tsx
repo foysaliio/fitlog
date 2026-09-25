7d2356c467b307eb15f738ee31e0774522df07a3
Foysal Hossien <foysalhossien77@gmail.com>
2026-09-25 18:16:26 +0600

    perf: cache workout grid component output

--- src/components/home/WorkoutGrid.tsx
+++ src/components/home/WorkoutGrid.tsx
@@ -1,7 +1,14 @@
+import { cacheLife, cacheTag } from "next/cache";
+
 import { getWorkouts } from "@/lib/api";
 import WorkoutSearchGrid from "./WorkoutSearchGrid";
 
 const WorkoutGrid = async () => {
+  "use cache";
+
+  cacheLife("days");
+  cacheTag("workouts");
+
   const workouts = await getWorkouts();
 
   return <WorkoutSearchGrid workouts={workouts} />;
